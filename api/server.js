b40d6e6e76ffc4aca37b111ea6bf32035f11e089
Robert Shepherd <robertsh26@outlook.com>
2024-06-18 14:54:55 -0700

    we

--- api/server.js
+++ api/server.js
@@ -2,7 +2,30 @@
 
 const express = require('express')
 const User = require('./users/model')
+
 const server = express()
+server.use(express.json())
+
+server.post('/api/users', (req, res) => {
+    const user = req.body;
+    if (!user.name || !user.bio) {
+        res.status(400).json({
+            message: "Please provide name and bio for the user"
+        })
+    } else {
+    User.insert(user)
+    .then(createdUser => {
+        res.status(201).json(createdUser)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: 'error getting users',
+            err: err.message,
+            stack: err.stack,
+        })
+    })
+    }
+})
 
 server.get('/api/users', (req, res) => {
     User.find()
@@ -21,10 +44,15 @@ server.get('/api/users', (req, res) => {
 server.get('/api/users/:id', (req, res) => {
     User.findById(req.params.id)
     .then(user => {
+        if (!user) {
+            res.status(404).json ({
+                message: "The user with the specified ID does not exist"
+            })
+        }
         res.json(user)
     })
     .catch(err => {
-        res.status(500).json({
+        res.status(404).json({
             message: 'error getting user',
             err: err.message,
             stack: err.stack,
