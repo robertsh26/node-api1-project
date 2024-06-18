88493b8eda08b17ded02f73d722dd3dd02e53f3c
Robert Shepherd <robertsh26@outlook.com>
2024-06-18 15:24:17 -0700

    we

--- api/server.js
+++ api/server.js
@@ -6,6 +6,35 @@ const User = require('./users/model')
 const server = express()
 server.use(express.json())
 
+server.put('/api/users/:id', async (req, res) => {
+    try {
+        const possibleUser = await User.findById(req.params.id)
+    if(!possibleUser) {
+        res.status(404).json({
+            message: "The user with the specified ID does not exist"
+        })
+    } else {
+        if (!req.body.name || !req.body.bio) {
+            res.status(400).json({
+                message: "Please provide name and bio for the user"
+            })
+        } else {
+           const updatedUser = await User.update(
+            req.params.id, 
+            req.body
+        )
+           res.status(200).json(updatedUser)
+        }
+    }
+  } catch (err) {
+    res.status(404).json({
+        message: 'error updating user',
+        err: err.message,
+        stack: err.stack,
+   })
+  }
+})
+
 server.delete('/api/users/:id', async (req, res) => {
     try {
         const possibleUser = await User.findById(req.params.id)
@@ -19,9 +48,10 @@ server.delete('/api/users/:id', async (req, res) => {
     }
   } catch (err) {
     res.status(404).json({
-        message: 'error getting user',
+        message: 'error deleting user',
         err: err.message,
         stack: err.stack,
+   })
   }
 })
 
